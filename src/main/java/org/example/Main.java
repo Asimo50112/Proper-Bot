3bb5392c25c8c98057d0b58c6c876b092bf357f8
Asimo50112 <asimothegamer@gmail.com>
2026-01-06 10:00:57 -0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
-package org.example;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import java.io.*;
+import java.util.Properties;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+    private static final String CONFIG_FILE = "bot-token.properties";
+    private static String token;
+
+    public static void main(String[] args) {
+        handleConfig();
+
+        // Initialize JDA and register the new CommandHandler listener
+        JDA jda = JDABuilder.createDefault(token)
+                .addEventListeners(new CommandHandler()) // <--- Registering the class
+                .build();
+
+        // Define Global Commands
+        var globalCommands = jda.updateCommands().addCommands(
+            Commands.slash("echo", "Repeat a message")
+                .addOption(OptionType.STRING, "message", "The words to repeat", true)
+        );
+
+        globalCommands.queue();
+    }
+
+    private static void handleConfig() {
+        Properties prop = new Properties();
+        File file = new File(CONFIG_FILE);
+
+        if (!file.exists()) {
+            try (OutputStream output = new FileOutputStream(CONFIG_FILE)) {
+                prop.setProperty("bot_token", "YOUR_TOKEN_HERE");
+                prop.store(output, "Discord Bot Configuration");
+                System.out.println("Created bot-token.properties. Please add your token and restart.");
+                System.exit(0); 
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        } else {
+            try (InputStream input = new FileInputStream(CONFIG_FILE)) {
+                prop.load(input);
+                token = prop.getProperty("bot_token");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
