423eef9457881be932a2c1405016bd415ec8ac50
Asimo50112 <asimothegamer@gmail.com>
2026-01-06 10:03:57 -0800

    Update CommandHandler.java

--- src/main/java/org/example/CommandHandler.java
+++ src/main/java/org/example/CommandHandler.java
@@ -1,4 +1,47 @@
 package org.example;
 
-public class CommandHandler {
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandHandler extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
+        // 1. Identify which command was typed
+        String commandName = event.getName();
+
+        switch (commandName) {
+            case "echo":
+                handleEcho(event);
+                break;
+                
+            case "ping":
+                event.reply("Pong! 🏓").queue();
+                break;
+
+            default:
+                event.reply("Unknown command!").setEphemeral(true).queue();
+                break;
+        }
+    }
+
+    /**
+     * Logic for the /echo command
+     */
+    private void handleEcho(SlashCommandInteractionEvent event) {
+        // Retrieve the "message" option we defined in Main.java
+        OptionMapping messageOption = event.getOption("message");
+
+        if (messageOption == null) {
+            event.reply("You didn't provide a message!").setEphemeral(true).queue();
+            return;
+        }
+
+        String userMessage = messageOption.getAsString();
+        
+        // Reply back to the user
+        event.reply("You said: **" + userMessage + "**").queue();
+    }
 }
