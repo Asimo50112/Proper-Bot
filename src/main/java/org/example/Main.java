456472b8de516543a23b71481db8e7ac1cb5927f
asimo <asimothegamer@gmail.com>
2026-01-06 12:59:38 -0800

    Add new files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,63 +2,47 @@ package org.example;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 import java.io.*;
 import java.util.Properties;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static String token;
 
     public static void main(String[] args) throws Exception {
-        if (!loadConfig()) return;
+        if (!loadConfig()) {
+            System.out.println("CONFIG CREATED: Please put your token in bot-token.properties");
+            return;
+        }
 
         try {
-            ERLCVehicleGuard vehicleGuard = new ERLCVehicleGuard();
-
             JDA jda = JDABuilder.createDefault(token)
-                    .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT)
-                    .setMemberCachePolicy(MemberCachePolicy.ALL)
-                    .setChunkingFilter(ChunkingFilter.ALL) // Force bot to load every member
-                    .addEventListeners(
-                            new ERLCSetupCommand(),
-                            new ERLCRemoteCommand(),
-                            new JoinCommand(),
-                            new ERLCStatusCommand(),
-                            new ERLCPlayersCommand(),
-                            new PurgeCommand(),
-                            vehicleGuard 
-                    )
+                    .enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.MESSAGE_CONTENT)
+                    .setMemberCachePolicy(MemberCachePolicy.ALL) // Critical for role checks
                     .build()
                     .awaitReady();
 
-            // Slash Command Registration
+            // FIX 1: Pass 'jda' into the constructor
+            // This starts the 20-second monitor automatically
+            new ERLCVehicleGuard(jda);
+
+            // FIX 2: Manually add the command data since the static method was removed
+            // Note: If you have other command classes, add them here too
             jda.updateCommands().addCommands(
-                    ERLCSetupCommand.getCommandData(),
-                    ERLCRemoteCommand.getCommandData(),
-                    JoinCommand.getCommandData(),
-                    ERLCStatusCommand.getCommandData(),
-                    ERLCPlayersCommand.getCommandData(),
-                    PurgeCommand.getCommandData(),
-                    ERLCVehicleGuard.getCommandData()
+                    net.dv8tion.jda.api.interactions.commands.build.Commands.slash("vehicle-restrictions", "Vehicle restriction system")
+                            .addSubcommands(
+                                    new net.dv8tion.jda.api.interactions.commands.build.SubcommandData("add", "Restrict a car to a role")
+                                            .addOption(net.dv8tion.jda.api.interactions.commands.OptionType.STRING, "carname", "Exact PRC Car Name", true)
+                                            .addOption(net.dv8tion.jda.api.interactions.commands.OptionType.ROLE, "role", "Authorized Role", true),
+                                    new net.dv8tion.jda.api.interactions.commands.build.SubcommandData("scan", "Manually trigger scan")
+                            )
             ).queue();
 
-            // Background Loop: Scans every 20 seconds
-            ScheduledExecutorService scannerLoop = Executors.newSingleThreadScheduledExecutor();
-            scannerLoop.scheduleAtFixedRate(() -> {
-                for (Guild guild : jda.getGuilds()) {
-                    vehicleGuard.performScan(guild);
-                }
-            }, 10, 20, TimeUnit.SECONDS);
-
-            System.out.println("Bot Online. Scanner running every 20 seconds.");
-
-        } catch (Exception e) { e.printStackTrace(); }
+            System.out.println("Bot is online as: " + jda.getSelfUser().getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static boolean loadConfig() {
@@ -67,7 +51,7 @@ public class Main {
         try {
             if (!f.exists()) {
                 try (OutputStream o = new FileOutputStream(f)) {
-                    prop.setProperty("bot_token", "TOKEN_HERE");
+                    prop.setProperty("bot_token", "INSERT_TOKEN_HERE");
                     prop.store(o, null);
                 }
                 return false;
@@ -75,7 +59,7 @@ public class Main {
             try (InputStream i = new FileInputStream(f)) {
                 prop.load(i);
                 token = prop.getProperty("bot_token");
-                return token != null && !token.equals("TOKEN_HERE");
+                return token != null && !token.equals("INSERT_TOKEN_HERE");
             }
         } catch (IOException e) { return false; }
     }
